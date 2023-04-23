b18d760e13050b4b13a8b3ec285b7474a160259a
Alejandro Suárez Perales <62011966+asu4net@users.noreply.github.com>
2023-04-23 10:29:25 +0200

    Create premake5.lua

---
+++ premake5.lua
@@ -0,0 +1,44 @@
+project "imgui"
+	  kind "StaticLib"
+    language "C++"
+    cppdialect "C++20"
+    staticruntime "on"
+
+	  targetdir ("%{wks.location}/Binaries/" .. outputdir .. "/%{prj.name}")
+    objdir ("%{wks.location}/Intermediate/" .. outputdir .. "/%{prj.name}")
+	
+	files
+	{
+		"imconfig.h",
+		"imgui.h",
+		"imgui.cpp",
+		"imgui_draw.cpp",
+		"imgui_internal.h",
+		"imgui_widgets.cpp",
+		"imstb_rectpack.h",
+		"imstb_textedit.h",
+		"imstb_truetype.h",
+		"imgui_demo.cpp",
+		"imgui_tables.cpp"
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+		cppdialect "C++20"
+		staticruntime "On"
+
+	filter "system:linux"
+		pic "On"
+		systemversion "latest"
+		cppdialect "C++20"
+		staticruntime "On"
+
+  filter "configurations:Debug"
+    defines "NIT_DEBUG"
+    runtime "Debug"
+    symbols "on"
+    
+  filter "configurations:Release"
+    defines "NIT_RELEASE"
+    runtime "Release"
+    optimize "on"
